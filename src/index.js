229cb8de2bd9e7bf050fac5a1a77c0cf0ff2a0e0
Martin Feineis <martinfeineis@gmx.de>
2018-02-06 21:54:13 +0100

    Added process adapters and better separation of read- and write-side

--- src/index.js
+++ src/index.js
@@ -1,50 +1,65 @@
-const Stream = require("most");
 const { T, cond, equals, map, path, reduce } = require("ramda");
 
 const { assemble } = require("arch-one");
-const { Keyed, button, div, render } = require("arch-one/dom");
+const { SyncListAdapter, bindAdapter } = require("arch-one/process");
+const { Keyed, button, createRenderer, div, input, label } = require("arch-one/dom");
+
+const intents = {
+    INCREMENT: "intent/INCREMENT",
+    DO_SOMETHING: "intent/DO_SOMETHING",
+};
+
+const facts = {
+    INCREMENTED: "fact/INCREMENTED",
+};
 
 const handleIncrement = emit => model => evt => (
-    emit("intent/INCREMENT")
+    emit(intents.INCREMENT)
 );
 
 const handleSomething = emit => model => evt => (
-    emit("intent/DO_SOMETHING")
+    emit(intents.DO_SOMETHING, { some: "data" })
 );
 
-const SimpleLabel = plugins => ({ label = `Some label` }) => [
-    [div, { onClick: handleSomething }, label],
+const MainReadSide = () => (model = { checked: false, counter: 0 }) => cond([
+    [equals(facts.INCREMENTED), {
+        ...model,
+        counter: model.counter + 1,
+    }],
+    [T, model],
+]);
+
+const MainWriteLogic = () => ({
+    handleIntents: [
+        [intents.INCREMENT, emit => intent => [
+            emit(facts.INCREMENTED),
+        ]],
+    ],
+    processFacts: stateFact => queueIntent => fact => [
+    ],
+});
+const MainWriteSide = bindAdapter(SyncListAdapter, MainWriteLogic);
+
+const Checkbox = () => ({ checked = false }) => children => [
+    [label, { onClick: handleSomething },
+     [input, { checked }], ...children
+    ],
 ];
 
-const Main = plugins => ({
-    intents: [
-        {
-            type: "intent/INCREMENT",
-            handle: emit => intent => Stream.from([
-                emit("fact/INCREMENTED"),
-            ]),
-        },
-    ],
-    //process: spawnFact => spawnIntent => fact => Stream.from([
-    //]),
-    replay: (model = { counter: 0 }) => cond([
-        [equals("fact/INCREMENTED"), {
-            ...model,
-            counter: model.counter + 1,
-        }],
-        [T, model],
-    ]),
-});
-
-const DomDriver = ({ intl }) => {
+const MainView = ({ intl }) => {
     const KeyedButton = Keyed(button, "some-key");
-    return model => [
-        [SimpleLabel, { label: "Custom label" }],
-        [KeyedButton, { onClick: handleIncrement },
-            intl.translate("main.clicked.label", model.counter),
-            //`Clicked ${model.counter} times`,
+    return model => children => [
+        [Checkbox, `Custom label ${model.checked ? "(checked)" : ""}`
         ],
+        [KeyedButton, { onClick: handleIncrement },
+          intl.translate("main.clicked.label", model.counter),
+        ],
+        ...children,
     ];
 };
 
-render({ intl: key => key }, [Main, DomDriver], document.body);
+const Main = assemble(MainView, MainReadSide, MainWriteSide);
+
+const render = createRenderer(/*plugins:*/{ intl: key => key });
+
+render(Main, document.body);
